fe4a143dcb5788fc1dc01a2632d67dc22a7ec79e
MR-Stan <michaelrstanley87@gmail.com>
2019-10-01 17:27:10 -0500

    added basic html and javascript pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// create variable gameObject to hold all other items
+
+// set variables:
+
+// create character objects - name, health points, attack power, counter attack power, image
+    // Obi-Wan Kenobi
+    // Luke Skywalker
+    // Darth Sidious
+    // Darth Maul
+
+// attack power is iterative
+
+// counter attack power doesn't change
+
+// opponent counter attacks instantly
+
+// characters div holds four options initially - white background green border
+// select one and it moves to hero div 
+// the other three move to enemies div - red background with black lines
+// select one to fight which moves to defender div - black background with green border
+// once hp === 0 enemy disappears
+
+// only name and hp are displayed around character
+
+// must be able to win no matter what character is chosen
+
+// text at the bottom displays attack and counter damage and win / lose
