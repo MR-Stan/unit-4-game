1bd5bb842224fcdb76e6b1d806bc80c80a8a97f2
MR-Stan <michaelrstanley87@gmail.com>
2019-10-06 00:08:18 -0500

    winCount, lossCount, iterative attacks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
-var gameObject = {
-
+let gameObject = {
+// add battle log
 // set variables:
 // characters hero enemies defender
 
@@ -7,39 +7,40 @@ var gameObject = {
     characters : [
         {
             name: "Obi-Wan Kenobi",
-            hp: 120,
-            ap: 13,
-            ca: 9,
+            hp: 120, // health points
+            ap: 13, // attack power
+            ca: 9, // counter attack power
             img: "<img src='assets/images/obi.webp' alt='image of Obi-Wan Kenobi'>"
         },
 
         {
             name: "Luke Skywalker",
-            hp: 100,
-            ap: 20,
-            ca: 33,
+            hp: 100, // health points
+            ap: 20, // attack power
+            ca: 33, // counter attack power
             img: "<img src='assets/images/luke.jpg' alt='image of Luke Skywalker'>"
         },
 
         {
             name: "Darth Sidious",
-            hp: 150,
-            ap: 10,
-            ca: 14,
+            hp: 150, // health points
+            ap: 10, // attack power
+            ca: 14, // counter attack power
             img: "<img src='assets/images/darthsidious.webp' alt='image of Darth Sidious'>"
         },
 
         {
             name: "Darth Maul",
-            hp: 180,
-            ap: 16,
-            ca: 25,
+            hp: 180, // health points
+            ap: 16, // attack power
+            ca: 25, // counter attack power
             img: "<img src='assets/images/darthmaul.jpg' alt='image of Darth Maul'>"
         }
     ],
 
     heroHealth : 0,
     heroAttack : 0,
+    heroBase : 0,
     // heroCounter : 0, need to change to enemy
     enemyHealth : 0, // need to assign further down
 
@@ -75,12 +76,13 @@ var gameObject = {
     // need to have the option to switch hero
     // need button to move to next
     chooseHero : function() {
-        for (var i = 0; i < 1; i++) {
+        for (let i = 0; i < 1; i++) {
             $(document).on("click", ".character", function() {
                 console.log(this);
                 $(this).attr("class", "hero");
                 gameObject.heroHealth = ($(this).attr("data-hp"));
-                gameObject.heroAttack = ($(this).attr("data-ap"));
+                gameObject.heroBase = ($(this).attr("data-ap"));
+                gameObject.heroAttack = 0;
                 // gameObject.heroCounter = ($(this).attr("data-ca")); need to move enemy
                 $(".hero").appendTo("#attacker");
                 $("#status").text("You chose " + ($(this).attr("data-name")) + " as your hero.");
@@ -93,7 +95,7 @@ var gameObject = {
     },
     
     chooseEnemy : function() {
-        for (var i = 0; i < 1; i++) {
+        for (let i = 0; i < 1; i++) {
             $(document).on("click", ".enemy", function() {
                 $(this).attr("class", "defender");
                 $(".defender").appendTo("#defender");
@@ -107,15 +109,36 @@ var gameObject = {
         }
     },
 
+    // need to do something on first attack
     fight : function() {
+        let attack = parseInt(gameObject.heroAttack);
         $("#attackBtn").click(function() { 
-            $("#status").text("You attacked for " + gameObject.heroAttack + " damage!");
-            var attack = parseInt(gameObject.heroAttack);
-            attack = attack + attack;
-            gameObject.heroAttack = attack;
-            console.log(attack)
+            attack += parseInt(gameObject.heroBase);
+            $("#battleText").text("You attacked for " + attack + " damage!");
+            return attack;
         });
-    }
+    },
+
+    winCount : function() {
+        let winCounter = 0;
+        return function() {
+            winCounter += 1; 
+            //const winText = document.getElementById("wins");  
+            //winText.innerText = winCounter;
+            return winCounter;
+        }
+    },
+
+    lossCount : function() {
+        let lossCounter = 0;
+        return function() {
+            lossCounter += 1; 
+            //const lossText = document.getElementById("losses");  
+            //lossText.innerText = lossCounter;
+            return lossCounter;
+        }
+    },
+
 }
 
 // attack power is iterative
